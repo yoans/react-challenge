8f9dd7c6fdae6699bea3211e88b9d96b09082414
Nathaniel Young <nathanyoans@gmail.com>
2019-06-29 08:14:25 -0500

    show team name 1.3

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -11,24 +11,44 @@ class Main extends Component {
     constructor(props){
         super(props);
         this.state = {
-            players: []
+            players: [],
+            teams: [],
         };
         this.loadData();
     }
     
     loadData = async () => {
-        var options = {
+        var playersOptions = {
             uri: `${localhostServer}/players`,
             headers: {
                 'User-Agent': 'Request-Promise'
             },
             json: true
         };
-        const players = await requestPromise(options)
-            .catch(function (err) {
-                console.log({err});
-            });
-        this.setState({players})
+        const playersPromise = requestPromise(playersOptions)
+        
+        var teamsOptions = {
+            uri: `${localhostServer}/teams`,
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true
+        };
+        const teamsPromise = requestPromise(teamsOptions)
+        try {
+            const players = await playersPromise;
+            const teams = await teamsPromise;
+            console.log({
+                teams,
+                players
+            })
+            this.setState({
+                teams,
+                players
+            })
+        } catch (e){
+            console.log(e);
+        }
     }
 
     render() {
@@ -37,11 +57,12 @@ class Main extends Component {
                 <div style={styles.title}>NBA Interview</div>
                 <Search style={styles.search} />
                 {
-                    this.state.players.map((player)=>
+                    this.state.players.map((player, index)=>
                         <Card
+                            key = {index}
                             name={player.name}
                             image={`${localhostServer}/${player.image}`} 
-                            team={player.team} 
+                            team={this.state.teams.find(team=>team.id === player.team).name} 
                         />
                     )
                 }
