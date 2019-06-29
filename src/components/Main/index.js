e2a3bf492d2b2114c7872bca7e03766d0910eb60
Nathaniel Young <nathanyoans@gmail.com>
2019-06-29 08:38:30 -0500

    paginate 2.4

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,20 +10,33 @@ const localhostServer = 'http://localhost:3008';
 class Main extends Component {
     constructor(props){
         super(props);
+        const page = 1;
         this.state = {
             players: [],
             teams: [],
+            page,
         };
-        this.loadData();
+        this.loadData(page);
     }
-    
-    loadData = async () => {
+    setLastPage = (lastPage) => {
+        this.setState({lastPage});
+    }
+
+    loadData = async (page) => {
         var playersOptions = {
             uri: `${localhostServer}/players`,
+            qs: {
+                _page: `${page}`,
+            },
             headers: {
                 'User-Agent': 'Request-Promise'
             },
-            json: true
+            json: true,
+            transform: (body, response, resolveWithFullResponse) => {
+                const lastPage = response.headers.link.split(',').pop().match(/.*_page=(.*)>;.*/)[1]
+                this.setLastPage(lastPage)
+                return body;
+            }
         };
         const playersPromise = requestPromise(playersOptions)
         
@@ -38,10 +51,6 @@ class Main extends Component {
         try {
             const players = await playersPromise;
             const teams = await teamsPromise;
-            console.log({
-                teams,
-                players
-            })
             this.setState({
                 teams,
                 players
@@ -50,12 +59,29 @@ class Main extends Component {
             console.log(e);
         }
     }
+    prevPage = () =>{
+        const page = this.state.page - 1 <=0 ? 1 : this.state.page - 1;
+        
+        this.loadData(page);
+        this.setState({
+            page
+        })
+    }
+    nextPage = () =>{
+        const page = this.state.page + 1 >= this.state.lastPage ? this.state.lastPage : this.state.page + 1;
+        this.loadData(page);
+        this.setState({
+            page
+        })
+    }
 
     render() {
         return (
             <div style={{ ...styles.container, ...this.props.style }}>
                 <div style={styles.title}>NBA Interview</div>
                 <Search style={styles.search} />
+                <button onClick={this.prevPage}>Previous Page</button>
+                <button  onClick={this.nextPage}>Next Page</button>
                 {
                     this.state.players.map((player, index)=>
                         <Card
